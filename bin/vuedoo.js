6f49c291e7cde31749fbaea3f28e422d8caa42f0
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2018-03-25 11:14:10 -0400

    Use path resolve when determinings library directory

--- bin/vuedoo.js
+++ bin/vuedoo.js
@@ -1,8 +1,9 @@
 #! /usr/bin/env node
 
 const argv = require('minimist')(process.argv.slice(2))
+const path = require('path')
 
-process.env.VUEDOO_LIBRARY_DIRECTORY = process.mainModule.filename.replace('/bin/vuedoo.js', '')
+process.env.VUEDOO_LIBRARY_DIRECTORY = path.resolve(process.mainModule.filename, '../../')
 process.env.VUEDOO_PROJECT_DIRECTORY = process.cwd()
 
 switch (argv._[0]) {
